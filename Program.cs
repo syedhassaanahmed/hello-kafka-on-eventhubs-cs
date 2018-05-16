7f87cd0a324a0d64ba9ea158dad07c0e1b23b040
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2018-05-16 19:59:52 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloKafkaOnEventHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "<EVENTHUB_NAMESPACE>.servicebus.windows.net:9093" },
+                { "security.protocol", "SASL_SSL" },
+                { "sasl.mechanism", "PLAIN"},
+                { "group.id", "$Default"},
+                //{ "debug", "generic,broker,topic,metadata,feature,queue,protocol,msg,security,all" },
+                { "sasl.username", "$ConnectionString" },
+                { "sasl.password", "Endpoint=sb://<EVENTHUB_NAMESPACE>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<PRIMARY_KEY>" },
+                { "ssl.ca.location", "cacert.pem" },
+            };
+
+            using (var consumer = new Consumer<Null, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnMessage += (_, msg)
+                  => Console.WriteLine($"Read '{msg.Value}' from: {msg.TopicPartitionOffset}");
+
+                consumer.OnError += (_, error)
+                  => Console.WriteLine($"Error: {error}");
+
+                consumer.OnConsumeError += (_, msg)
+                  => Console.WriteLine($"Consume error ({msg.TopicPartitionOffset}): {msg.Error}");
+
+                consumer.Subscribe("kafka-events");
+
+                while (true)
+                {
+                    consumer.Poll(TimeSpan.FromMilliseconds(100));
+                }
+            }
+        }
+    }    
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hello-kafka-on-eventhubs-cs
-Connecting to a Kafka-enabled Azure Event Hub using Confluent's .NET Client
+This repo demonstrates how to connect to a [Kafka-enabled Azure Event Hubs](https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-for-kafka-ecosystem-overview) using [Confluent's .NET Client](https://github.com/confluentinc/confluent-kafka-dotnet).
+
+## Caveat
+[This doc](https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-quickstart-kafka-enabled-event-hubs#send-and-receive-messages-with-kafka-in-event-hubs) describes how to provide auth config for connecting a Java-based Kafka Producer/Consumer. However the setting `sasl.jaas.config` won't work with Confluent's .NET Client and will throw an exception:
+
+```
+Unhandled Exception: System.ArgumentException: Java JAAS configuration is not supported, see https://github.com/edenhill/librdkafka/wiki/Using-SASL-with-librdkafka for more information.
+```
+
+## Solution
+Instead of using `sasl.jaas.config`, use `sasl.username` and `sasl.password`. Furthermore on  Windows, default trusted root CA certs are stored in Windows Registry. They're not automatically discovered by Confluent client. You'll need to obtain these from somewhere else, hence I've used the `cacert.pem` file distributed with [curl](https://curl.haxx.se/ca/cacert.pem), set its build setting to "Always copy to output directory" and reference it in the `ssl.ca.location`.
+
+## Reference
+[ConfluentCloud's C# example](https://github.com/confluentinc/confluent-kafka-dotnet/blob/17004b179df7fcde38e12062391b60c9a37d4a41/examples/ConfluentCloud/Program.cs#L51)
